a34a92b0832297130356f9233ccd45163234d3ca
Fardin <143270493+FardinBazregar@users.noreply.github.com>
2024-01-29 19:11:18 +0330

    this is Show the character of the sentence
    this is Show the character of the sentence

--- app.js
+++ app.js
@@ -1,58 +1,19 @@
-let value = "";
-let counter = 1;
-const inputHanlder = (e) => {
-  value = e.target.value;
-};
-const resetCounter = () => {
-  const todoItems = document.getElementsByClassName("todo-item");
-  let usedNumbers = [];
-  for (let i = 0; i < todoItems.length; i++) {
-    const text = todoItems[i].innerText;
-    const number = parseInt(text.split(".")[0], 10);
-    usedNumbers.push(number);
-  }
+const showSentence = (sentece) => {
+  let obj = {};
+  const word = [...sentece];
 
-  for (let i = 1; ; i++) {
-    if (!usedNumbers.includes(i)) {
-      counter = i;
-      break;
+  word.forEach((value) => {
+    if (!obj[value]) {
+      obj[value] = 1;
+    } else {
+      obj[value] += 1;
     }
-  }
-};
-
-const inputElement = document
-  .getElementById("input")
-  .addEventListener("input", inputHanlder);
-
-const btnHandler = (e) => {
-  e.stopPropagation();
-  if (document.getElementsByClassName("todo-item").length >= 4) {
-    alert("The maximum number of items has reached 4!");
-    return;
-  }
-
-  const newTodoElement = document.createElement("div");
-  newTodoElement.classList.add("todo-item");
-
-  const deletBtn = document.createElement("button");
-  deletBtn.innerText = "❌";
-  deletBtn.classList.add("delete-button");
-  deletBtn.addEventListener("click", () => {
-    newTodoElement.classList.add("removed");
-    setTimeout(() => {
-      newTodoElement.remove();
-      resetCounter();
-    }, 1000);
   });
-
-  newTodoElement.innerText = `${counter}. ${value}`;
-  newTodoElement.appendChild(deletBtn);
-  counter++;
-
-  document.getElementById("todoValue").appendChild(newTodoElement);
-  document.getElementById("input").value = "";
+  return obj;
 };
 
-const btn = document
-  .getElementById("btn")
-  .addEventListener("click", btnHandler);
+console.log(
+  showSentence(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magni debitis atque consequatur quae harum."
+  )
+);
